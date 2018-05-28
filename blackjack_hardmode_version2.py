a3c550c5231cf17ec9370f10ab8786ff023914ef
jhassler1031 <jhassler1031@gmail.com>
2018-05-28 11:44:53 -0400

    mostly working, issue with breaking out of split when first hand gets 21 or bust

--- blackjack_hardmode_version2.py
+++ blackjack_hardmode_version2.py
@@ -1,5 +1,25 @@
 
-from Blackjack_classes import *
+#Create a game of Blackjack
+#Actors: Card, Deck, Player, Dealer, Shoe, Hand
+
+#Card - hold info about the card, suite, type, and value
+
+#Deck - holds a group of Cards, able to deliver a single called at a time, shuffles those cards when created
+#   Collab: Card is an association, will be used by Player
+
+#Player - will have a hand made up of cards taken from the deck, given to it by the Dealer.  Needs to be able to calculate
+#the value of the hand and have the option to hit (draw another card) or stay.
+#   Collab: Card is an association for the hand, dependancy on Dealer for cards
+
+#Dealer - subtype of Player which has the additional function of dealing cards
+
+#Rules:
+#object of the game is to get closer to 21 than the dealerself.
+#if you get 21 you auto win
+#if less than 21 you can hit or stay
+#if you go over 21 you auto lose
+
+from blackjack_classes import *
 
 wants_to_play = True
 player = Player()
@@ -11,10 +31,11 @@ deck = Shoe()
 while wants_to_play:
 
     #Initialize player hands and game control variables
-    player.hand = []
-    dealer.hand = []
-    player_turn = 1
+    player.hands = [Hand()]
+    dealer.hands = [Hand()]
+    #player_turn = 1
     player_state = True
+    hand_state = True
     player_win = False
     player_bust = False
     dealer_win = False
@@ -25,6 +46,8 @@ while wants_to_play:
         deck = Shoe()
 
     print(f"Player has ${player.money} in the bank.")
+
+    #Begins a new game
 
     while True:
 
@@ -38,12 +61,16 @@ while wants_to_play:
                 print("That is not a valid bet amount.")
 
         #Deal a hand
-        dealer.deal_hand(player)
+        dealer.deal_hand(player, deck)
 
         print("Dealer's top card: ")
-        print(dealer.hand[0])
+        print(dealer.hands[0].cards[0])
         print()
 
+        print("Player's hand: ")
+        player.print_hands()
+
+        #Check if player wants insurance
         want_ins = ""
 
         if dealer.has_ace():
@@ -58,14 +85,18 @@ while wants_to_play:
                         print("Invalid amount.")
                 player.buy_ins(ins_amount)
 
-        #check for 21 at the beginning
-        if player.check_21() and dealer.check_21():
+        #check dealer for 21 and insurance payout if purchased
+        if player.hands[0].check_21() and dealer.hands[0].check_21():
             print("It's a draw.")
-            break
-        elif player.check_21():
+            if want_ins.lower() == "y":
+                player.pay_ins()
+            break  #breaks from game loop
+        elif player.hands[0].check_21():
             player.player_wins()
+            if want_ins.lower() == "y":
+                player.lose_ins()
             break
-        elif dealer.check_21():
+        elif dealer.hands[0].check_21():
             if want_ins.lower() == "y":
                 player.pay_ins()
             dealer.player_wins(player)
@@ -76,84 +107,53 @@ while wants_to_play:
 
         #Player's turn
 
-        print("Player's turn. You have: ")
-        print(player)
+        #check for split
+        for count in range(player.hand_count):
+            if player.can_split(count):
+                split_input = input("Would you like to split? Y or N ")
+                if split_input.lower() == "y":
+                    player.split_hand(deck)
+                    print("Player's hands: ")
+                    player.print_hands()
+
+        #Check for double down or surrender
+        player_input = input("""
+Would the player like to double down (1)
+Surrender (2))
+Play on (Enter):
+""")
+        if player_input == "1":   #player doubled down
+            player.double_down()
+        elif player_input == "2":   #player surrenders
+            player.surrender()
+            player_state = False
+
+        #Play hand(s)
 
         while player_state:
+            hand_state = True
+            for count in range(player.hand_count):
+                print("Current hand: ")
+                print(player.hands[count])
+                while hand_state:
+                    player_input = input("Enter 1 to hit or Enter to stand: ")
 
-            player_split = False
+                    hand_state = player.player_turn(player_input, count, deck)
 
-            if player_turn == 1:
-                #if player has duplicate cards, offer to split
-                if player.can_split():
-                    split_input = input("Would you like to split? Y or N ")
-                    if split_input.lower() == "y":
-                        player.split_hand(deck)
-                        player_split = True
+                    if player.hands[count].check_21():
+                        player.player_wins()
+                        player_win = True
+                        break
+                    elif player.hands[count].check_bust():
+                        dealer.player_wins(player)
+                        player_bust = True
+                        break
+                    print("You have: ")
+                    print(player)
+            break
 
-                else:
-
-                    player_input = input("""
-1 to hit,
-2 to double down,
-3 to surrender,
-Enter to stand: """)
-
-
-            else:
-                player_input = input("Enter 1 to hit or any to stand: ")
-
-            player_state = player.player_turn(player_input, deck)
-
-            if player_input == "3":
-                break
-
-            if player.check_21():
-                player.player_wins()
-                player_win = True
-                break
-            elif player.check_bust():
-                dealer.player_wins(player)
-                player_bust = True
-                break
-            print("You have: ")
-            print(player)
-
-            player_turn += 1
 
         #End of player's turn
-
-        if player_win:
-            break
-        elif player_bust:
-            break
-        elif player_input == "3":
-            break
-
-#if player split, need to run through playing with split hand
-
-        #start of split hand
-        if player_split:
-
-            player_state = True
-
-            player_input = input("Enter 1 to hit or any to stand: ")
-
-            player_state = player.player_turn(player_input, deck)
-
-            if player.check_21():
-                player.player_wins()
-                player_win = True
-                break
-            elif player.check_bust():
-                dealer.player_wins(player)
-                player_bust = True
-                break
-            print("You have: ")
-            print(player)
-
-
-        #End of player's split turn
 
         if player_win:
             break
@@ -171,11 +171,11 @@ Enter to stand: """)
 
         while player_state:
             player_state = dealer.player_turn(deck)
-            if dealer.check_21():
+            if dealer.hands[0].check_21():
                 dealer.player_wins(player)
                 dealer_win = True
                 break
-            elif dealer.check_bust():
+            elif dealer.hands[0].check_bust():
                 player.player_wins()
                 dealer_bust = True
                 break
@@ -189,8 +189,8 @@ Enter to stand: """)
         #End of Dealer's turn
 
         #Everyone has stayed, need to compare
-
-        dealer.compare_hands(player)
+        for count in range(player.hand_count):
+            dealer.compare_hands(player, count)
         break
 
     #Game over, check if the player would like to play again, and has funds to do so
